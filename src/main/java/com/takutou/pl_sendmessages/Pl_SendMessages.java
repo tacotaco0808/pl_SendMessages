7b4f63206820fbedcc7ebc87c50655acd5e66ba1
TakutoMatsui <takuto1488@gmail.com>
2024-06-27 12:32:09 +0900

    first commit minecraft_plugin

---
+++ src/main/java/com/takutou/pl_sendmessages/Pl_SendMessages.java
@@ -0,0 +1,18 @@
+package com.takutou.pl_sendmessages;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pl_SendMessages extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("Hello World!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
